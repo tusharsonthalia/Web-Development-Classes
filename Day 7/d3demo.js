c5771a4a1433a4e83692d1c3e6b4a40a89b126c4
tusharsonthalia <sonthaliatushar@gmail.com>
2020-08-03 12:09:19 +0530

    day 7 update

--- Day 7/d3demo.js
+++ Day 7/d3demo.js
@@ -27,6 +27,7 @@ a.text("Changed p1 only");
 Example 3: Selection Functions use modifications
 */
 
+/*
 var a = d3.select("#div1 #p1");
 a.attr("style", "color:red;");
 
@@ -40,4 +41,37 @@ function show() {
     var d = d3.select("#txt-1");
     var userInput = d.property("value");
     c.append("h2").text(userInput);
+}
+*/
+
+/*
+Example 4: D3 Transitions
+*/
+
+/*
+var a = d3.selectAll("#container").on("mouseover", function() {
+    d3.select(this).transition()
+    .duration(100)
+    .style("border-radius", "100px")
+    .style("background", "orange")
+});
+
+var a = d3.selectAll("#container").on("mouseout", function() {
+    d3.select(this).transition()
+    .duration(100)
+    .style("border-radius", "0px")
+    .style("background", "gray")
+});
+*/
+
+/*
+Example 5: D3 selection functions - joining functions
+*/
+
+function show() {
+    var names = [{name: "Tushar"}, {name: "Vedant"}];
+    var a = d3.selectAll("#div1 p");
+    a.data(names).text(function(d) {
+        return this.innerText + " " + d.name;
+    }).exit().remove();
 }
