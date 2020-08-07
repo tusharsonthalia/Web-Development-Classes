c423bd55192b4456ab99c076ca77d18d212f5fd6
tusharsonthalia <sonthaliatushar@gmail.com>
2020-08-07 11:53:03 +0530

    building the react app

---
+++ #6. REACTJS/Day 10/my-app/src/Student.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+
+class Student extends Component {
+    render() {
+        return React.createElement('h1', null, 'Welcome To My First React App');
+    }
+}
+
+export default Student;
---
+++ #6. REACTJS/Day 10/my-app/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Student from './Student'
+
+
+ReactDOM.render(<Student/>, document.getElementById('root'));
