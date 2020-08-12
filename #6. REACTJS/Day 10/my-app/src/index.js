c49a1900566e9c568697c308dbb058dd1653575c
tusharsonthalia <sonthaliatushar@gmail.com>
2020-08-12 21:09:39 +0530

    updated the folder structure

--- #6. REACTJS/Day 10/my-app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Student from './Student'
-import Example6 from './Example6'
-
-
-
-
-ReactDOM.render(<Example6/>, document.getElementById('root'));
